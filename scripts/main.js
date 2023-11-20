29fa70f6335e8c6ebf92a87e20ce53c1ba4b1726
dthcs <dthcsc@gmail.com>
2023-11-21 01:12:36 +0900

    update AI Korea

--- scripts/main.js
+++ scripts/main.js
@@ -25,34 +25,34 @@ function removeElmentFromArray (arr, element){
      delay = value
  }
 
- function randomInteger(upper){
-     let number = Math.floor(Math.random()*upper)
-     return number
- }
+//  function randomInteger(upper){
+//      let number = Math.floor(Math.random()*upper)
+//      return number
+//  }
 
- function createBlackMaze(){
-    for(var c=0; c<tileColumnCount; c++){
-        for(var r=0; r<tileRowCount; r++){
-            tiles[c][r].state = "wall"
-        }
-    }
- }
+//  function createBlackMaze(){
+//     for(var c=0; c<tileColumnCount; c++){
+//         for(var r=0; r<tileRowCount; r++){
+//             tiles[c][r].state = "wall"
+//         }
+//     }
+//  }
 
- function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+//  function shuffle(array) {
+//     var currentIndex = array.length, temporaryValue, randomIndex;
   
-    // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
+//     // While there remain elements to shuffle...
+//     while (0 !== currentIndex) {
   
-      // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
+//       // Pick a remaining element...
+//       randomIndex = Math.floor(Math.random() * currentIndex);
+//       currentIndex -= 1;
   
-      // And swap it with the current element.
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
-    }
+//       // And swap it with the current element.
+//       temporaryValue = array[currentIndex];
+//       array[currentIndex] = array[randomIndex];
+//       array[randomIndex] = temporaryValue;
+//     }
   
-    return array;
-  }
+//     return array;
+//   }
